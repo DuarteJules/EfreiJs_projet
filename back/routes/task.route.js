4d1fcba2e27f38fed7bba0c28576275e79b671fd
Jules <jules.duarte@edu.esiee-it.fr>
2024-09-17 15:20:56 +0200

    init du back

---
+++ back/routes/task.route.js
@@ -0,0 +1,36 @@
+const express = require( "express" );
+const task = require('../controllers/task.controllers')
+
+module.exports =  (app) => {
+
+    const router = express.Router();
+
+    router.get('/', (req, res) => {
+        res.send('yo')
+    })
+
+    router.get('/hello', (req, res) => {
+        res.send('Bonjour')
+    })
+
+    router.get('/return-json', (req, res) => {
+        res.send(JSON.stringify({
+            name : "JD"
+        }))
+    })
+
+    router.get('/getTask', [], task.getTasks)
+
+    router.get('/getTask/:id', [], task.getTaskById)
+
+    router.get('/getUsers', [], task.getUsers)
+
+    router.post('/createTask', [], task.addTask)
+
+    router.post('/deleteTask/:id', [], task.removeTask)
+
+    router.post('/updateTask/:id',[], task.updateTask)
+
+    app.use("/task", router)
+
+}
